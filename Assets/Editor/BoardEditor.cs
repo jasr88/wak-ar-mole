d7522b724e612c95cc4b8b7b66b47b1a05de39c3
Alex Silva <alex.silva@poktapok.studio>
2020-04-30 11:27:33 -0500

    STATUS:      * Complete CHANGES:      * Change the anidatet "For" loop for a more natural way yo instantiate the holes      * Instantiate Prefabs instead of GO, so the modifications to the prefab affect all the holes in the scene      * Modification of the Sprite Hole => "Dafault Hole" 512*512 px

--- Assets/Editor/BoardEditor.cs
+++ Assets/Editor/BoardEditor.cs
@@ -1,19 +1,41 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using WakARmole;
 
-[CustomEditor(typeof(Board))]
-public class BoardEditor:Editor {
-	public override void OnInspectorGUI() {
-		base.OnInspectorGUI ();
+namespace WakARmole {
+	[CustomEditor (typeof (Board))]
+	public class BoardEditor :Editor {
+		public override void OnInspectorGUI() {
+			base.OnInspectorGUI ();
 
-		Board board = (Board)target;
-		if (GUILayout.Button("Generate Holes")) {
-			board.GenerateBoard ();
+			Board board = (Board)target;
+			if (GUILayout.Button ("Create Hole Prefab From Template") && board.IsHolePrefabEmpty()) {
+				Hole.CreateHoleInstance ();
+			}
+
+			if (!board.IsHolePrefabEmpty ()) {
+				if (GUILayout.Button ("Generate Holes")) {
+					board.GenerateBoard ();
+				}
+
+				if (GUILayout.Button ("Reset board")) {
+					board.ResetBoard ();
+				}
+			}
+
 		}
 
-		if (GUILayout.Button ("Reset board")) {
-			board.ResetBoard ();
+		[MenuItem ("GameObject/WakARmole/Board", false, 10)]
+		public static void CreateBoardInstance() {
+			Board.CreateBoardInstance ();
+		}
+
+
+		[MenuItem ("GameObject/WakARmole/Hole", false, 10)]
+		public static Hole CreateHoleInstance() {
+			return Hole.CreateHoleInstance ();
 		}
 	}
+
 }
+
+
--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace WakARmole {
+	[ExecuteInEditMode]
 	public class Board :MonoBehaviour {
-		[SerializeField]
 		private GameObject boardGO;
 		[SerializeField]
-		private Hole holePrefab;
+		private Hole holePrefab = null;
 		[SerializeField]
 		[Range (-1, 1)] private float paddingX = 0;
 		[SerializeField]
@@ -14,7 +15,21 @@ namespace WakARmole {
 
 		private List<Hole> holes = new List<Hole> ();
 
+		private void OnEnable() {
+			Debug.Log ("Enable Board");
+			SetBoardGo ();
+		}
+
+		private void SetBoardGo() {
+			Debug.Log (transform.GetChild (0).gameObject.name);
+			boardGO = transform.GetChild (0).gameObject;
+		}
+
 		// This method is only for run on the custom editor
+		public bool IsHolePrefabEmpty() {
+			return holePrefab == null;
+		}
+
 		public void ResetBoard() {
 			while (boardGO.transform.childCount != 0) {
 				DestroyImmediate (boardGO.transform.GetChild (0).gameObject);
@@ -22,21 +37,18 @@ namespace WakARmole {
 		}
 		
 		public void GenerateBoard() {
-			ResetBoard ();
-			if (boardGO == null) {
-				Debug.LogError ("You must to assign a GameObject to representante the virtual board for the game, please verify your Board prefab and assign a suitable 3D model for the boardGO property");
-				return;
-			}
-
 			if (holePrefab == null) {
-				Debug.LogError ("You must to assign a Hole Object to representante the virtual holes on the board, please verify your Board prefab and assign a suitable prefab for the holePrefab property");
+				Debug.LogError ("You must to assign a Hole Prefab to representante the virtual holes on the board, please verify your Board prefab and assign a suitable prefab for the holePrefab property");
 				return;
 			}
+		
+			ResetBoard ();
+	
 			Renderer boardMesh = boardGO.GetComponent<Renderer> ();
 			Vector2 holeSize = new Vector2 (
-				holePrefab.GetComponentInChildren<Renderer> ().bounds.size.x + paddingX,
-				holePrefab.GetComponentInChildren<Renderer> ().bounds.size.z + paddingZ
-				);
+				holePrefab.Size.x + paddingX,
+				holePrefab.Size.y + paddingZ
+			);
 
 			int xCount = Mathf.FloorToInt (boardMesh.bounds.size.x / holeSize.x);
 			int zCount = Mathf.FloorToInt (boardMesh.bounds.size.z / holeSize.y);
@@ -48,15 +60,21 @@ namespace WakARmole {
 			float yOffset = boardMesh.bounds.size.y + 0.01f;
 			float zOffset = holeSize.y / 2.0f;
 
-			for (int x = 0; x < xCount; x++) {
-				for (int z = 0; z < zCount; z++) {
-					GameObject hole = Instantiate (holePrefab.gameObject);
+			for (int z = 0; z < zCount; z++) {
+				for (int x = 0; x < xCount; x++) {
+					GameObject hole = PrefabUtility.InstantiatePrefab (holePrefab.gameObject) as GameObject;
 					hole.transform.localPosition = new Vector3 (((x - xCount / 2.0f) * cellWidth) + xOffset, yOffset, ((z - zCount / 2.0f) * cellHeght) + zOffset);
 					hole.transform.parent = boardGO.transform;
 					hole.name = "Hole (" + x + "," + z + ")";
 				}
 			}
 		}
+
+		public static void CreateBoardInstance() {
+			GameObject board =Instantiate(Resources.Load ("BoardContainer",typeof(GameObject)), Vector3.zero,Quaternion.identity) as GameObject;
+			board.name = "Board Template";
+			board.GetComponent<Board> ().SetBoardGo ();
+		} 
 
 	}
 }
